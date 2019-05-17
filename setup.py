94c529d780b7fdc410d18789c5e416870821da72
Tong <tongli.bioinfo@gmail.com>
2019-05-17 18:55:18 +0200

    added easy run

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
         name='bfseg',
-        version='0.0.2',
+        version='0.0.3',
         description='a segmentation algorithm to find cell borders in bright-field images',
         long_description=long_description,
         long_description_content_type='text/markdown',
